a676298b6afb12710fe27fcbcee8853b104b6e35
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2023-08-15 22:54:15 +0530

    5th Commit

--- Assignment5.java
+++ Assignment5.java
@@ -10,16 +10,17 @@ class Assignment5{
         final String RESET = "\033[0m";
 
         final String DASHBOARD = "Welcome to Smart Banking";
-        final String CREATE_NEW_ACCOUNT = "Create New Account";
-        final String DEPOSITS = "Deposits";
-        final String WITHDROWAL = "Withdrawal";
-        final String TRANSFER = "Transfer";
+        final String CREATE_NEW_ACCOUNT = "Open New Account";
+        final String DEPOSITS = "Deposit Money";
+        final String WITHDROWAL = "Withdraw Money";
+        final String TRANSFER = "Transfer Money";
         final String CHECK_ACCOUNT_BALANCE = "Check Account Balance";
-        final String DELETE_ACCOUNT = "Delete Account";
+        final String DELETE_ACCOUNT = "Drop Existing Account";
 
         final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
         final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
 
+        String[] accountId= new String[0];
         String screen = DASHBOARD;
 
         do{
@@ -31,17 +32,48 @@ class Assignment5{
             switch(screen){
                 case DASHBOARD:
                     System.out.println("\t[1]. Create New Account");
-                    System.out.println("\t[2]. Deposits");
-                    System.out.println("\t[3]. Withdrawal");
-                    System.out.println("\t[4]. Transfer");
+                    System.out.println("\t[2]. Deposit Money");
+                    System.out.println("\t[3]. Withdraw Money");
+                    System.out.println("\t[4]. Transfer Money");
                     System.out.println("\t[5]. Check Account Balance");
-                    System.out.println("\t[6]. Delete Account");
+                    System.out.println("\t[6]. Drop Existing Account");
                     System.out.println("\t[7]. Exit\n");
                     System.out.print("\tEnter an option to continue: ");
                     int option = SCANNER.nextInt();
                     SCANNER.nextLine();
 
-                default: continue;
+                    switch (option){
+                        case 1: screen = CREATE_NEW_ACCOUNT; break;
+                        case 2: screen = DEPOSITS; break;
+                        case 3: screen = WITHDROWAL; break;
+                        case 4: screen = TRANSFER; break;
+                        case 5: screen = CHECK_ACCOUNT_BALANCE ; break;
+                        case 6: screen = DELETE_ACCOUNT; break;
+                        case 7: System.out.println(CLEAR); System.exit(0);
+                        default: continue;
+                    }
+                    break;
+
+                case CREATE_NEW_ACCOUNT:
+
+                    // ID Validation
+                    String[] tempId = new String[accountId.length+1];
+
+                    String accountIdName =String.format("New Account ID: SDB%05d \n", (accountId.length + 1));
+                    for(int i = 0;i<accountId.length;i++){
+                        tempId[i]=accountId[i];
+                    }
+                    tempId[tempId.length-1]=accountIdName;
+                    accountId=tempId;
+
+                    // Name Validation
+
+                    String name;
+                    boolean valid;
+            
+                
+
+                default: System.exit(0);
 
             }
             
